6e9fd244e60cc924fc360aa6e567b99b587f9a84
rocketboy001 <adam@adambyers.com>
2014-10-18 14:24:11 -0700

    Rewrite to support sensors
    Rewrote to support temp and voltage sensors.

--- arduino_house_side/arduino_house_side.ino
+++ arduino_house_side/arduino_house_side.ino
@@ -1,10 +1,10 @@
 /**********************************************************************************************************************
 Mailbox Notifier - House Side
 
-This sketch was written to work as a Snail mail box delivery notification system. It uses to LEDs mounted to the mailbox
-as local (to the mailbox) delivery notifications. It also sends out a serial character via a connected XBee modem. When 
-the receiving XBee (set in a remote location like in your house) receives the character it either turns an indicator
-light on or off.
+This sketch was written to work as a Snail mail delivery notification system. It uses two LEDs mounted to the mailbox
+as local (to the mailbox) delivery notification. It also sends out delivery/retrieve data via an XBee modem (serial). When 
+the receiving XBee (set in a remote location, like your house) receives the data it turns a delivery indicator
+light on or off and records and displays the delivery/retrieve data.
 
 For more information see - http://adambyers.com/2013/11/mailbox-notifier/ or ping adam@adambyers.com
 
@@ -12,25 +12,376 @@ All code (except external libraries and third party code) is published under the
 
 **********************************************************************************************************************/
 
+//Libraries
+#include "Wire.h"
+#include "RTClib.h"
+#include "LiquidCrystal.h"
+
+RTC_DS1307 rtc; // Define the RTC
+
 // PIN Names
-int LightSW = 2;
+int lightSW = 2;
+
+// LCD PINs
+LiquidCrystal lcd(3, 4, 5, 6, 7, 8);
+
+// Vars
+float temp = 0;
+float voltage = 0;
+int notify = 3; // This will come in via serial as 0 or 1. Set to 3 here to keep it from triggering.
+// Current data/time vars
+int cYear = 0;
+int cMonth = 0;
+int cDay = 0;
+int cHour = 0;
+int cMin = 0;
+int cSecond = 0;
+// Delivery vars
+float dTemp = 0;
+float dVoltage = 0;
+int dYear = 0;
+int dMonth = 0;
+int dDay = 0;
+int dHour = 0;
+int dMin = 0;
+int dSecond = 0;
+// Retrieve vars
+float rTemp = 0;
+float rVoltage = 0;
+int rYear = 0;
+int rMonth = 0;
+int rDay = 0;
+int rHour = 0;
+int rMin = 0;
+int rSecond = 0;
+// LCD update vars
+long prevLCDMillis = 0;
+long lcdInt = 2000; // Time to display each "screen."
+int numScreen = 9; // How many "screens" we have, less 1.  
+int screenNum = 0;  
+bool screenChanged = true;
+// Misc Vars
+bool delivered = false;
+bool retrieve = false;
+
+// LCD update states 
+#define C_date 0
+#define C_time 1
+#define D_date 2
+#define D_time 3
+#define D_temp 4
+#define D_voltage 5
+#define R_date 6
+#define R_time 7
+#define R_temp 8
+#define R_voltage 9
 
 void setup() {
-  pinMode(LightSW, OUTPUT);
+  pinMode(lightSW, OUTPUT);
+  
   Serial.begin(9600);
+  
+  Wire.begin();
+  
+  rtc.begin();
+  
+  lcd.begin(8, 2);
+  
+/*
+To set the date/time uncomment the line below and upload the sketch. The RTC will be set to the system date/time recorded when 
+compiled. After, comment out the line and upload the sketch again. Form this point forward the RTC will keep track of the date/time
+itself. Failure to comment out the line and re-upload will cause the date/time to be reset to the compiled time each time the Arduino 
+is rebooted.
+*/
+  
+// rtc.adjust(DateTime(F(__DATE__), F(__TIME__)));
+  
 }
 
 void loop() {
-  while(Serial.available()){  // Determine if there is anything to read from serial
+  DateTime now = rtc.now(); // Get the current time & date for display only.
+  
+  // Record the current time and date for display only.
+  cYear = (now.year()) - 2000; // Subtract 2000 so we get a 2-digit date and can fit it on the LCD.
+  cMonth = (now.month());
+  cDay = (now.day());
+  cHour = (now.hour());
+  cMin = (now.minute()); 
+  cSecond = (now.second());
+  
+  /*
+  Data from the mailbox is sent to the house via serial in the format t75.00v3.45n1 where t=temperature v=voltage n=notify
+  The following reads the incoming serial data and parses it based on the indicator (t,v,n) preceding each value. 
+  */
+  
+  while(Serial.available()) {  // Determine if there is anything to read from serial.
+    if (Serial.find("t")) // Find the 't' char that denotes where the Temp reading begins.
+      temp = Serial.parseFloat(); // Parse the float # after the 't' char to get the temp reading.
     
-    char LightState = Serial.read();  // If there is something to read from serial, read it
+    if (Serial.find("v"))  // Find the 'v' char that denotes where the Voltage reading begins.
+      voltage = Serial.parseFloat(); // Parse the float # after the 'v' char to get the voltage reading.
+      
+    if (Serial.find("n"))  // Find the 'n' char that denotes where the Notify state begins.
+      notify = Serial.parseInt(); // Parse the int # after the 'n' char to get the Notify state. 
+  }
 
-      if(LightState == '0'){  	 
-        digitalWrite(2, LOW);
-      }
-	
-      else if(LightState == '1'){
-        digitalWrite(2, HIGH);
-      }
+  /*
+  Data is from the mailbox is transmitted 4 times to insure that the house received the data (since it's transmitting blindly, 
+  i.e. there is no handshake or received confirmation). We only need one good transmission though and can assume that once we 
+  know what the notify setting is we've received a good transmission and can stop listening. We do this by setting 
+  'delivered == true' and 'retrieve == true' after we've run each sequence.   
+  */
+
+  if (notify == 1 && delivered == false) { // If mail has been delivered and we have not run this sequence already.
+    digitalWrite(lightSW, HIGH); // Turn on the notification light.
+    
+    DateTime now = rtc.now(); // Get the current time & date.
+    
+    // Record the delivery time and date
+    dYear = (now.year()) - 2000; // Subtract 2000 so we get a 2-digit date and can fit it on the LCD.
+    dMonth = (now.month());
+    dDay = (now.day());
+    dHour = (now.hour());
+    dMin = (now.minute()); 
+    dSecond = (now.second());
+    
+    // Record the delivery temp and voltage.
+    dTemp = temp;
+    dVoltage = voltage;
+    
+    // We assume that since mail is being delivered that it has not yet been retrieved and reset the retrieve vars.
+    rYear = 0;
+    rMonth = 0;
+    rDay = 0;
+    rHour = 0;
+    rMin = 0; 
+    rSecond = 0;
+    rTemp = 0;
+    rVoltage = 0;
+    
+    retrieve = false; // Reset
+    delivered = true; // Don't run this again till it's false.
+  }
+  
+  if (notify == 0 && retrieve == false) { // If mail has been retrieved and we have not run this sequence already.
+    digitalWrite(lightSW, LOW); // Turn off the notification light.
+    
+    DateTime now = rtc.now(); // Get the current time & date.
+
+    // Record the retrieve time and date.
+    rYear = (now.year()) - 2000; // Subtract 2000 so we get a 2-digit date and can fit it on the LCD.
+    rMonth = (now.month());
+    rDay = (now.day());
+    rHour = (now.hour());
+    rMin = (now.minute()); 
+    rSecond = (now.second());
+    
+    // Record the retrieve temp and voltage.
+    rTemp = temp;
+    rVoltage = voltage; 
+  
+    retrieve = true;
+    delivered = false;  
+  }
+
+  // Screen update - based off code from 'robtillaart' in the Arduino forums.
+  unsigned long currentLCDMillis = millis();
+  
+  if (currentLCDMillis - prevLCDMillis > lcdInt) {
+    prevLCDMillis = currentLCDMillis; 
+    screenNum++;
+    if (screenNum > numScreen) screenNum = 0;  // If all defined screens have been shown, start over.
+      screenChanged = true;
+  }
+
+  if (screenChanged == true) {
+    screenChanged = false; // Reset for next round.
+    switch(screenNum)    
+    {
+      case C_date: 
+        showCdate(); 
+      break;
+      case C_time: 
+        showCtime(); 
+      break;
+      case D_date: 
+        showDdate(); 
+      break;
+      case D_time: 
+        showDtime(); 
+      break;
+      case D_temp: 
+        showDtemp();
+      break;
+      case D_voltage:
+        showDvoltage();
+      break;
+      case R_date: 
+        showRdate(); 
+      break;
+      case R_time: 
+        showRtime(); 
+      break;
+      case R_temp: 
+        showRtemp();
+      break;
+      case R_voltage:
+        showRvoltage();
+      break;
     }
+  }
+}
+
+// Functions
+void showCdate() {
+  lcd.clear();
+  lcd.setCursor(0, 0);           
+  lcd.print("Cur Date");
+  lcd.setCursor(0, 1);          
+  lcd.print(cMonth);
+  lcd.print("/");
+  lcd.print(cDay);
+  lcd.print("/");
+  lcd.print(cYear);
+}
+
+void showCtime() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Cur Time");
+  lcd.setCursor(0, 1);
+    
+  if (cHour < 10) {
+    lcd.print("0");
+  }
+    
+  lcd.print(cHour);
+  lcd.print(":");
+ 
+  if (cMin < 10) {
+    lcd.print("0");
+  }
+ 
+  lcd.print(cMin);
+  lcd.print(":");
+    
+  if (cSecond < 10) {
+    lcd.print("0");
+  }
+ 
+  lcd.print(cSecond);
+}
+
+void showDdate() {
+  lcd.clear();
+  lcd.setCursor(0, 0);           
+  lcd.print("Del Date");
+  lcd.setCursor(0, 1);          
+  lcd.print(dMonth);
+  lcd.print("/");
+  lcd.print(dDay);
+  lcd.print("/");
+  lcd.print(dYear);
+}
+
+void showDtime() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Del Time");
+  lcd.setCursor(0, 1);
+    
+  if (dHour < 10) {
+    lcd.print("0");
+  }
+    
+  lcd.print(dHour);
+  lcd.print(":");
+ 
+  if (dMin < 10) {
+    lcd.print("0");
+  }
+ 
+  lcd.print(dMin);
+  lcd.print(":");
+    
+  if (dSecond < 10) {
+    lcd.print("0");
+  }
+ 
+  lcd.print(dSecond);
+}
+
+void showDtemp() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Del Temp");
+  lcd.setCursor(0, 1);
+  lcd.print(dTemp);
+  lcd.print((char)223);
+}
+
+void showDvoltage() {
+  lcd.clear();
+  lcd.setCursor(0, 0);           
+  lcd.print("Del Volt");
+  lcd.setCursor(0, 1);          
+  lcd.print(dVoltage);
+  lcd.print("v");
+}
+
+void showRdate() {
+  lcd.clear();
+  lcd.setCursor(0, 0);           
+  lcd.print("Rev Date");
+  lcd.setCursor(0, 1);          
+  lcd.print(rMonth);
+  lcd.print("/");
+  lcd.print(rDay);
+  lcd.print("/");
+  lcd.print(rYear);
+}
+
+void showRtime() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Rev Time");
+  lcd.setCursor(0, 1);
+  
+  if (rHour < 10) {
+    lcd.print("0");
+  }
+    
+  lcd.print(rHour);
+  lcd.print(":");
+ 
+  if (rMin < 10) {
+    lcd.print("0");
+  }
+ 
+  lcd.print(rMin);
+  lcd.print(":");
+    
+  if (rSecond < 10) {
+    lcd.print("0");
+  }
+ 
+  lcd.print(rSecond);
+}
+
+void showRtemp() {
+  lcd.clear();
+  lcd.setCursor(0, 0);
+  lcd.print("Rev Temp");
+  lcd.setCursor(0, 1);
+  lcd.print(rTemp);
+  lcd.print((char)223);
+}
+
+void showRvoltage() {
+  lcd.clear();
+  lcd.setCursor(0, 0);           
+  lcd.print("Rev Volt");
+  lcd.setCursor(0, 1);          
+  lcd.print(rVoltage);
+  lcd.print("v");
 }
--- arduino_mailbox_side/arduino_mailbox_side.ino
+++ arduino_mailbox_side/arduino_mailbox_side.ino
@@ -1,117 +1,180 @@
 /**********************************************************************************************************************
 Mailbox Notifier - Mailbox Side
 
-This sketch was written to work as a Snail mail box delivery notification system. It uses two LEDs mounted to the mailbox
-as local (to the mailbox) delivery notification. It also sends out a serial character via an XBee modem. When 
-the receiving XBee (set in a remote location, like your house) receives the character it either turns an indicator
-light on or off.
+This sketch was written to work as a Snail mail delivery notification system. It uses two LEDs mounted to the mailbox
+as local (to the mailbox) delivery notification. It also sends out delivery/retrieve data via an XBee modem (serial). When 
+the receiving XBee (set in a remote location, like your house) receives the data it turns a delivery indicator
+light on or off and records and displays the delivery/retrieve data.
 
 For more information see - http://adambyers.com/2013/11/mailbox-notifier/ or ping adam@adambyers.com
 
 All code (except external libraries and third party code) is published under the MIT License.
 
 **********************************************************************************************************************/
-
-#include <avr/sleep.h>
-#include <avr/interrupt.h>
+// Libraries
+#include "avr/sleep.h"
+#include "avr/interrupt.h"
+#include "Wire.h"
+#include "OneWire.h"
+#include "DallasTemperature.h"
 
 // PIN Names
-int DeliverSW = 2;
-int RetrieveSW = 3;
-int XBPower = 4;
-int NotifyLED1 = 5;
-int NotifyLED2 = 6;
-int DomeLED = 7;
+int deliverSW = 2;
+int retrieveSW = 3;
+int xbPower = 4;
+int notifyLED1 = 5;
+int notifyLED2 = 6;
+int domeLED = 7;
+int battery = A0;
 
-// Vars
-bool delivery = false;
-int transmitCount = 0;
+// Misc vars
+int notify = 0; // The notify state, 1=delivered 0=retrieved
+int xbWakeupDelay = 1000; // Number of milliseconds we wait after waking the XBee before using it.
+int transmitTimes = 4; // How many times we transmit out the delivery/retrieve data. More than once to make sure it's retrieved.
+int xbTransmitDelay = 6000; // Number of milliseconds we wait between XBee transmissions, so the transmissions are not stepping on each other.
+int battPinReading = 0; // Holds the raw reading from the battery monitor pin.
+float battPinVoltage = 0; // Holds the raw pin voltage value.
+float battVoltage = 0; // Calculated battery voltage.
+float dividerRatio = 2.242; // Voltage divider ratio.
+float tempC = 0; // Mailbox temp reading in celsius.
+float tempF = 0; // Mailbox temp in fahrenheit.
+byte keepADCSRA; // Keeps the value of the analog digital converter so we can re-enable it when waking up.
+bool delivery = false; 
 
-/* 
-timesToTransmit is the number of times we will send out the ON or OFF signal out the XBee for the house notification. This can be reduced or increased depending on the distance/interference of the environment.
-We want to keep it as low as possible to conserve power.
-*/
-int timesToTransmit = 2;
-
-int XBeeWakeupDelay = 1000; // Number of milliseconds we wait after waking the XBee before using it.
-int XBeeTransmitDelay = 5500; // Number of milliseconds we wait between transmissions. I waited till the activity light on the receiving XBee went off and padded it a bit to get the 5.5 seconds here.
+// DS18B20 setup
+#define tempSensor 8 // PIN the DS18B20 is connected to.
+OneWire oneWire(tempSensor);
+DallasTemperature sensors(&oneWire); // Pass the OneWire reference to the DS18B20.
 
 void setup() { 
-  // Set PINs as inputs or outputs.
-  pinMode(DeliverSW, INPUT);
-  pinMode(RetrieveSW, INPUT);
-  pinMode(XBPower, OUTPUT);
-  pinMode(NotifyLED1, OUTPUT);
-  pinMode(NotifyLED2, OUTPUT);
-  pinMode(DomeLED, OUTPUT);
+  // Set unused digital pins to outputs and set them low to reduce power consumption when awake.
+  for (int unusedPINs = 9; unusedPINs < 13; unusedPINs++) {
+    pinMode(unusedPINs, OUTPUT);
+    digitalWrite(unusedPINs, LOW);
+  }
   
-  // Enable internal resistors by setting the PINs as HIGH. This is so we don't have to use pesky external resistors.
-  digitalWrite(DeliverSW, HIGH);
-  digitalWrite(RetrieveSW, HIGH);
+  // Set used PINs as inputs or outputs.
+  pinMode(deliverSW, INPUT);
+  pinMode(retrieveSW, INPUT);
+  pinMode(xbPower, OUTPUT);
+  pinMode(notifyLED1, OUTPUT);
+  pinMode(notifyLED2, OUTPUT);
+  pinMode(domeLED, OUTPUT);
+  pinMode(battery, INPUT);
   
-  // Start your serial engines!
-  Serial.begin(9600);  
+  // Enable internal so we don't have to use pesky external resistors for the switches.
+  digitalWrite(deliverSW, HIGH);
+  digitalWrite(retrieveSW, HIGH);
+  
+  Serial.begin(9600); // Start your serial engines!
+  
+  sensors.begin(); // Start the DallasTemperature library.
 }
 
-void loop() { 
-  digitalWrite(XBPower, HIGH); // Put the XBee to sleep.
-  delay(500); // Wait a half-second and then go to sleep.
-  F_sleep();
+/*
+As soon as the Arduino is powered on it goes into the main loop where it runs the sleep function that puts the XBee to sleep, 
+turns off brown-out, turns off the ADC, and then puts itself to sleep until mail is delivered. If the retrieve door is opened 
+before mail is delivered the Arduino wakes up, checks to see if 'delivery = true' before it runs the retrieved sequence. If 
+the retrieve door is opened and 'delivery = false' then the Arduino goes right back to sleep. This is the same if mail has been 
+delivered 'delivery = true' and the delivery door is opened before mail has been retrieved.
+*/
+
+void loop() {  
+  F_sleep(); // Go to sleep.
 }
 
+// Functions
 void F_sleep(void) {
-  attachInterrupt(0, F_Interrupt, RISING); // Attach the interrupt so that we can wake up when mail is delivered.
-  attachInterrupt(1, F_Interrupt, RISING); // Attach the interrupt so that we can wake up when mail is retrieved.
+  attachInterrupt(0, F_interrupt, RISING); // Attach the delivery interrupt so that we can wake up when mail is delivered.
+  attachInterrupt(1, F_interrupt, RISING); // Attach the retrieve interrupt so that we can wake up when mail is retrieved.
+
+  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Most power savings in this sleep mode.
+  sleep_enable(); // Enable sleep.
   
-  delay(500); // Slight delay for sanity
+  digitalWrite(xbPower, HIGH); // Put the XBee to sleep.
   
-  set_sleep_mode(SLEEP_MODE_PWR_DOWN); // Most power savings in this mode.
+  // Turn off the ADC
+  keepADCSRA = ADCSRA; // Record the current state of the ADCSRA
+  ADCSRA = 0; // Turn off the ADC
+
+  // Turn off brown-out enable in software
+  MCUCR = _BV (BODS) | _BV (BODSE);  // Turn on brown-out enable select.
+  MCUCR = _BV (BODS); // Must be done within 4 clock cycles of above.
+  
   sleep_mode(); // Go to sleep
+  
   sleep_disable(); // When woken up will continue to process from this point.
+  
+  ADCSRA = keepADCSRA; // Turn on the ADC
   
   F_process(); // Figure out what switch woke up the Arduino and act accordingly.
 }
   
-void F_Interrupt() {
+void F_interrupt() {
 }
 
 void F_process() { 
-  if (digitalRead(DeliverSW) == HIGH && delivery == false) {
-    digitalWrite(NotifyLED1, HIGH); // Turn the external notification LEDs on.
-    digitalWrite(NotifyLED2, HIGH); // Turn the external notification LEDs on.
+  if (digitalRead(deliverSW) == HIGH && delivery == false) { // Only run if mail has not been delivered yet. Otherwise, go back to sleep.
+    digitalWrite(notifyLED1, HIGH); // Turn ON the external notification LEDs.
+    digitalWrite(notifyLED2, HIGH); // Turn ON the external notification LEDs.
     
-    digitalWrite(XBPower, LOW); // Turn the radio on.
-    delay(XBeeWakeupDelay); // Give the radio some time to settle after waking up.
+    digitalWrite(xbPower, LOW); // Turn ON the XBee.
+    delay(xbWakeupDelay); // Give the XBee some time to settle after waking up.
     
-    // For the sake of making sure the house notification gets its ON signal we send the ON command out, wait 1.5 seconds and then send it out again until transmitCount is equal to timesToTransmit.
-    for (int t = 0; t != timesToTransmit; t++) {
-      Serial.println("1"); // Send a character out the serial to turn on the house notification.
-      delay(XBeeTransmitDelay); // Give some time for the transmission to end before we send the next one.
-    }
+    notify = 1; // Set the notify to 1 (1=delivered).
+    
+    getSensorReadings(); // Get the temp and batt voltage.
+    
+    transmitData(); // Send the notify, temp, and batt data out serial via the XBee.
     
     delivery = true; // Set this so we know the mail has been delivered and don't run the delivery sequence again before the retrieve door is opened.
 }
   
-  if (digitalRead(RetrieveSW) == HIGH && delivery == true) {
-    digitalWrite(NotifyLED1, LOW); // Turn off the external notification LEDs.
-    digitalWrite(NotifyLED2, LOW); // Turn off the external notification LEDs.
+  if (digitalRead(retrieveSW) == HIGH && delivery == true) { // Only run if mail has been delivered yet. Otherwise, go back to sleep.
+    digitalWrite(notifyLED1, LOW); // Turn OFF the external notification LEDs.
+    digitalWrite(notifyLED2, LOW); // Turn OFF the external notification LEDs.
    
-    // Turn on the dome LED on until the retrieve door is closed so we can see inside the mailbox if it's dark outside.
-    while (digitalRead(RetrieveSW) == HIGH) {
-      digitalWrite(DomeLED, HIGH);
+    // Turn ON the dome LED on until the retrieve door is closed so we can see inside the mailbox if it's dark outside.
+    while (digitalRead(retrieveSW) == HIGH) {
+      digitalWrite(domeLED, HIGH);
     } 
     
-    digitalWrite(DomeLED, LOW); // Turn the dome LED off.
+    digitalWrite(domeLED, LOW); // Turn the dome LED off.
     
-    digitalWrite(XBPower, LOW); // Wake up the XBee.
-    delay(XBeeWakeupDelay); // Give the XBee some time to settle after waking up before doing anything with it.
+    digitalWrite(xbPower, LOW); // Wake up the XBee.
+    delay(xbWakeupDelay); // Give the XBee some time to settle after waking up before doing anything with it.
     
-    // For the sake of making sure the house notification gets its OFF signal we send the OFF command out, wait 1.5 seconds and then send it out again until transmitCount is equal to timesToTransmit.
-    for (int t = 0; t != timesToTransmit; t++){
-      Serial.println("0"); // Send a character out the serial to turn on the house notification.
-      delay(XBeeTransmitDelay); // Give some time for the transmission to end before we send the next one.
-    }
+    notify = 0; // Set the notify to 0 (0=delivered).
+    
+    getSensorReadings(); // Get the temp and batt voltage.
+    
+    transmitData(); // Send the notify, tem, and batt data out the serial via XBee.
     
     delivery = false; // Set this bak to false so we get notified when mail is delivered again.
+  }
+}
+
+void getSensorReadings() {
+  // Get the battery voltage.
+  battPinReading = analogRead(battery);
+  battPinVoltage = battPinReading * 0.00322; // 3.3V / 1024 divisions = 0.00322 volts per division.
+  battVoltage = battPinVoltage * dividerRatio;
+    
+  // Get the temp.
+  sensors.requestTemperatures(); // Tell the sensor to record the temp to its memory.
+  tempC = sensors.getTempCByIndex(0); // Retrieve the temp from the sensor's memory and store it in a local var.
+  tempF = (tempC * 9.0)/ 5.0 + 32.0; // Convert to fahrenheit.
+}
+
+void transmitData() {
+  // For the sake of making sure the house notification gets its ON signal we send the ON command out, wait 5.5 seconds and then send it out again until transmitCount is equal to timesToTransmit.
+  for (int t = 0; t != transmitTimes; t++) {
+    Serial.print("t");
+    Serial.print(tempF);
+    Serial.print("v");
+    Serial.print(battVoltage);
+    Serial.print("n");
+    Serial.print(notify);
+    delay(xbTransmitDelay); // Give some time for the transmission to end before we send the next one.
   }
 }
